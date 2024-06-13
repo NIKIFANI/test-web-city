f0ded4bbd7f70ec48936281d9675eda89f056bf3
NIKIFANI <losik2000.777@gmail.com>
2024-06-13 12:16:23 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { GridComponent } from './components/grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
